0137a5e332d3537ca04cd566a6cc96d8e713fa48
nlepe7838 <ni963890@ucf.edu>
2026-02-25 17:19:55 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import RepetitionExercise from "./components/RepetitionExercise";
+import DurationExercise from "./components/DurationExercise";
 
 function App() {
+  const [exercise, setExercise] = useState(null);
+  const [type, setType] = useState(null);
+
+  if (type === "repetition") {
+    return <RepetitionExercise name={exercise} />;
+  }
+
+  if (type === "duration") {
+    return <DurationExercise name={exercise} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>Exercise Menu</h1>
+
+      <br />
+
+      <button onClick={() => {
+        setExercise("Push Ups");
+        setType("repetition");
+      }}>
+        Push Ups
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => {
+        setExercise("Running");
+        setType("duration");
+      }}>
+        Running
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => {
+        setExercise("Plank");
+        setType("duration");
+      }}>
+        Plank
+      </button>
     </div>
   );
 }
